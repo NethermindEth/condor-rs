53a085e5996d01a8891e396a76b9505287954bb6
Alex Abdugafarov <alexander.abdugafarov@nethermind.io>
2025-01-15 15:30:30 +0500

    Squashed commits to introduce workflows

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
 pub fn say_hello() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_say_hello() {
+        say_hello();
+    }
+
+    #[test]
+    fn test_for_workflow() {
+        use base64::prelude::*;
+        let _ = BASE64_STANDARD.encode([1,2,3,4,5]);
+        assert_eq!(true, true)
+    }
+}
