f8c4c91e48d5782ea1d79d2b25f2fd492beccdb3
Alex Abdugafarov <alexander.abdugafarov@nethermind.io>
2025-01-15 15:31:47 +0500

    Testing workflow

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
 pub fn say_hello() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_say_hello() {
+        say_hello();
+    }
+
+    #[test]
+    fn test_for_workflow() {
+        assert_eq!(true, false)
+    }
+}
