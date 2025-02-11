48fd4ed164577b625ae3cd3ba8cf85ee905c8236
mattsuffern <mateosuffern@gmail.com>
2025-02-11 12:05:02 -0300

    fixing linter_check

--- labrador/src/lib.rs
+++ labrador/src/lib.rs
@@ -6,19 +6,14 @@
 #![forbid(unsafe_code)]
 
 #![doc = include_str!("../../doc/mainpage-doc.md")]
-
 // Arithmetic Circuit Translation
 #![doc = include_str!("../../doc/arithmetic_circuit_translation.md")]
-
 // Ajtai Commitment
 #![doc = include_str!("../../doc/ajtai_commitment.md")]
-
 // Projections
 #![doc = include_str!("../../doc/projections.md")]
-
 // Aggregation
 #![doc = include_str!("../../doc/aggregation.md")]
-
 // Amortization
 #![doc = include_str!("../../doc/amortization.md")]
 
