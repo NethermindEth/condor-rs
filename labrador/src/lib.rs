9cc60d37473b42f961d580544e87804d2024c0ea
Maksim Ryndin <maksim.ryndin@gmail.com>
2025-02-20 11:56:50 +0000

    remove say_hello and base64 dependency

--- labrador/src/lib.rs
+++ labrador/src/lib.rs
@@ -17,25 +17,3 @@
 #![doc = include_str!("../doc/amortization.md")]
 
 pub mod zq;
-
-/// Prints a "Hello, world!" message
-pub fn say_hello() {
-    println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_say_hello() {
-        say_hello();
-    }
-
-    #[test]
-    fn test_for_workflow() {
-        use base64::prelude::*;
-        let _ = BASE64_STANDARD.encode([1, 2, 3, 4, 5]);
-        assert_eq!(true, true)
-    }
-}
