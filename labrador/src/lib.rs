117170d61ad7d080cd15eeac045786880d355d29
maksimryndin <maksim.ryndin@nethermind.io>
2025-02-21 11:36:09 +0100

    docs in ci (#23)

--- labrador/src/lib.rs
+++ labrador/src/lib.rs
@@ -4,38 +4,16 @@
 
 #![forbid(unsafe_code)]
 #![deny(clippy::as_conversions)]
-#![doc = include_str!("../../doc/mainpage-doc.md")]
+#![doc = include_str!("../doc/mainpage-doc.md")]
 // Arithmetic Circuit Translation
-#![doc = include_str!("../../doc/arithmetic_circuit_translation.md")]
+#![doc = include_str!("../doc/arithmetic_circuit_translation.md")]
 // Ajtai Commitment
-#![doc = include_str!("../../doc/ajtai_commitment.md")]
+#![doc = include_str!("../doc/ajtai_commitment.md")]
 // Projections
-#![doc = include_str!("../../doc/projections.md")]
+#![doc = include_str!("../doc/projections.md")]
 // Aggregation
-#![doc = include_str!("../../doc/aggregation.md")]
+#![doc = include_str!("../doc/aggregation.md")]
 // Amortization
-#![doc = include_str!("../../doc/amortization.md")]
+#![doc = include_str!("../doc/amortization.md")]
 
 pub mod zq;
-
-/// Prints a "Hello, world!" message
-pub fn say_hello() {
-    println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_say_hello() {
-        say_hello();
-    }
-
-    #[test]
-    fn test_for_workflow() {
-        use base64::prelude::*;
-        let _ = BASE64_STANDARD.encode([1, 2, 3, 4, 5]);
-        assert_eq!(true, true)
-    }
-}
--- labrador/src/main.rs
+++ labrador/src/main.rs
@@ -1,8 +1,6 @@
-use labrador::say_hello;
 use labrador::zq::Zq;
 
 fn main() {
-    say_hello();
     let a = Zq::new(5);
     let b = Zq::new(3);
     println!("a + b = {}", a + b);
