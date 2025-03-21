1a47fd67d4e301e46c53245814ccde42cf797474
mattsuffern <mateosuffern@gmail.com>
2025-03-21 11:18:10 -0300

    reformulating contraints doc

--- falcon_labrador/src/lib.rs
+++ falcon_labrador/src/lib.rs
@@ -6,6 +6,7 @@
 #![deny(clippy::as_conversions)]
 #![doc = include_str!("../../doc/falcon_labrador_docs/mainpage-doc.md")]
 #![doc = include_str!("../../doc/falcon_labrador_docs/Changing-Modulus-Norm-Checks.md")]
+#![doc = include_str!("../../doc/falcon_labrador_docs/reformulating-constraints.md")]
 
 ///Example function
 pub fn add(left: u64, right: u64) -> u64 {
